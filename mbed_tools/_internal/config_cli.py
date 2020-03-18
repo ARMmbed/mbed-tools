b1b80241ee8ea7db103c57dfaa3dd99104b7618e
Bartek Mucha <muchzill4@gmail.com>
2020-03-18 09:39:30 +0000

    Make mypy happy

--- mbed_tools/_internal/config_cli.py
+++ mbed_tools/_internal/config_cli.py
@@ -1,17 +1,23 @@
 """Exposes a click command which prints information about configuration variables of all child mbed packages."""
 import click
+import pdoc
+from typing import (
+    Iterable,
+    List,
+    cast,
+)
 
 from mbed_devices.mbed_tools import config_variables as mbed_devices_config_variables
 
 
 @click.command()
-def cli():
+def cli() -> None:
     """Prints information about configuration variables of all child mbed packages."""
     click.echo(_build_output(_gather_configuration_variables()))
 
 
-def _gather_configuration_variables():
-    return mbed_devices_config_variables
+def _gather_configuration_variables() -> List[pdoc.Variable]:
+    return cast(List[pdoc.Variable], mbed_devices_config_variables)
 
 
 _OUTPUT_PREAMBLE = """All the configuration variables can be set either via environment variables or
@@ -30,11 +36,11 @@ by any values previously set in your environment.
 """
 
 
-def _build_output(config_variables):
+def _build_output(config_variables: Iterable[pdoc.Variable]) -> str:
     variables_outputs = [f"{v.name}\n\n{_tab_prefix(v.docstring)}" for v in config_variables]
     variables_output = "\n\n".join(variables_outputs)
     return f"{_OUTPUT_PREAMBLE}\n\n{variables_output}"
 
 
-def _tab_prefix(mutiline_text):
+def _tab_prefix(mutiline_text: str) -> str:
     return "\n".join([f"\t{line}" for line in mutiline_text.split("\n")])
