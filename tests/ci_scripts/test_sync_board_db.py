8f5c9c7fb9963a5383ac536551e82cd96bc7c17b
Robert Walton <rob.walton@arm.com>
2020-08-17 13:44:20 +0100

    ci_scripts.sync_board_db: Write list of modified boards to news file

--- tests/ci_scripts/test_sync_board_db.py
+++ tests/ci_scripts/test_sync_board_db.py
@@ -90,6 +90,78 @@ BOARD_2 = {
     },
 }
 
+BOARD_3 = {
+    "type": "target",
+    "id": "3",
+    "attributes": {
+        "features": {
+            "mbed_enabled": ["Advanced"],
+            "mbed_os_support": [
+                "Mbed OS 5.10",
+                "Mbed OS 5.11",
+                "Mbed OS 5.12",
+                "Mbed OS 5.13",
+                "Mbed OS 5.14",
+                "Mbed OS 5.15",
+                "Mbed OS 5.7",
+                "Mbed OS 5.8",
+                "Mbed OS 5.9",
+            ],
+            "antenna": ["Connector"],
+            "certification": ["CE (Europe)", "FCC/CFR (USA)", "IC RSS (Canada)", "NCC (Taiwan)"],
+            "communication": ["LoRa"],
+            "interface_firmware": ["DAPLink"],
+            "security": ["TRNG"],
+            "target_core": ["Cortex-M4"],
+            "mbed_studio_support": ["Build and run"],
+        },
+        "board_type": "ADV_WISE_1510",
+        "flash_size": 256,
+        "name": "Advantech WISE-1510",
+        "product_code": "0458",
+        "ram_size": 64,
+        "target_type": "module",
+        "hidden": False,
+        "device_name": "STM32L443RC",
+        "slug": "advantech-wise-1510",
+    },
+}
+
+BOARD_2_MODIFIED = {
+    "type": "target",
+    "id": "2",
+    "attributes": {
+        "features": {
+            "mbed_enabled": ["Advanced"],
+            "mbed_os_support": [
+                "Mbed OS 6.2",
+                "Mbed OS 5.10",
+                "Mbed OS 5.11",
+                "Mbed OS 5.12",
+                "Mbed OS 5.13",
+                "Mbed OS 5.14",
+                "Mbed OS 5.15",
+                "Mbed OS 5.8",
+                "Mbed OS 5.9",
+            ],
+            "antenna": ["Connector"],
+            "certification": ["AS/NZS (Australia and New Zealand)", "CE (Europe)", "FCC/CFR (USA)", "IC RSS (Canada)"],
+            "communication": ["LoRa"],
+            "interface_firmware": ["DAPLink"],
+            "target_core": ["Cortex-M3"],
+            "mbed_studio_support": ["Build and run"],
+        },
+        "board_type": "MTB_MTS_XDOT",
+        "flash_size": 256,
+        "name": "Multitech xDOT",
+        "product_code": "0453",
+        "ram_size": 64,
+        "target_type": "module",
+        "hidden": False,
+        "device_name": "STM32L151CC",
+    },
+}
+
 
 def _make_mbed_boards_for_diff(boards_a, boards_b):
     return (
@@ -116,6 +188,24 @@ class TestDetermineBoardDatabaseUpdateResult(TestCase):
         self.assertEqual(len(result.boards_added), 0, "Expect no boards to be added to offline db.")
         self.assertEqual(len(result.boards_removed), 1, "Expect one board to be removed from offline db.")
 
+    def test_detects_modified(self):
+        mock_offline_boards, mock_online_boards = _make_mbed_boards_for_diff(
+            [BOARD_1, BOARD_2], [BOARD_1, BOARD_2_MODIFIED]
+        )
+        result = sync_board_database.determine_board_database_update_result(mock_offline_boards, mock_online_boards)
+        self.assertEqual(len(result.boards_modified), 1)
+        self.assertEqual(len(result.boards_added), 0)
+        self.assertEqual(len(result.boards_removed), 0)
+
+    def test_detects_added_removed_and_modified(self):
+        mock_online_boards, mock_offline_boards = _make_mbed_boards_for_diff(
+            [BOARD_3, BOARD_2], [BOARD_1, BOARD_2_MODIFIED]
+        )
+        result = sync_board_database.determine_board_database_update_result(mock_offline_boards, mock_online_boards)
+        self.assertEqual(len(result.boards_modified), 1)
+        self.assertEqual(len(result.boards_added), 1)
+        self.assertEqual(len(result.boards_removed), 1)
+
     def test_returns_empty_when_no_change(self):
         mock_offline_boards, mock_online_boards = _make_mbed_boards_for_diff([BOARD_1], [BOARD_1])
         result = sync_board_database.determine_board_database_update_result(mock_offline_boards, mock_online_boards)
@@ -132,7 +222,7 @@ class TestCreateNewsFileTextFromResult(TestCase):
         text = sync_board_database.create_news_file_text_from_result(result)
         self.assertEqual(
             text,
-            f"Targets added: {BOARD_1['attributes']['name']}, {BOARD_2['attributes']['name']}.\n",
+            f"Targets added: {BOARD_2['attributes']['name']}, {BOARD_1['attributes']['name']}.\n",
             "Text is formatted correctly.",
         )
 
@@ -144,18 +234,31 @@ class TestCreateNewsFileTextFromResult(TestCase):
         text = sync_board_database.create_news_file_text_from_result(result)
         self.assertEqual(
             text,
-            f"Targets removed: {BOARD_1['attributes']['name']}, {BOARD_2['attributes']['name']}.\n",
+            f"Targets removed: {BOARD_2['attributes']['name']}, {BOARD_1['attributes']['name']}.\n",
             "Text is formatted correctly.",
         )
 
-    def test_text_formatting_for_boards_added_and_removed(self):
-        mock_online_boards, mock_offline_boards = _make_mbed_boards_for_diff([BOARD_1], [BOARD_2])
+    def test_text_formatting_for_boards_modified(self):
+        mock_online_boards, mock_offline_boards = _make_mbed_boards_for_diff([BOARD_2], [BOARD_2_MODIFIED])
+        result = sync_board_database.determine_board_database_update_result(
+            online_boards=mock_online_boards, offline_boards=mock_offline_boards
+        )
+        text = sync_board_database.create_news_file_text_from_result(result)
+        self.assertEqual(
+            text, f"Targets modified: {BOARD_2['attributes']['name']}.\n", "Text is formatted correctly.",
+        )
+
+    def test_text_formatting_for_boards_added_removed_and_modified(self):
+        mock_online_boards, mock_offline_boards = _make_mbed_boards_for_diff(
+            [BOARD_3, BOARD_2], [BOARD_1, BOARD_2_MODIFIED]
+        )
         result = sync_board_database.determine_board_database_update_result(
             online_boards=mock_online_boards, offline_boards=mock_offline_boards
         )
         text = sync_board_database.create_news_file_text_from_result(result)
         self.assertEqual(
             text,
-            f"Targets added: {BOARD_1['attributes']['name']}.\nTargets removed: {BOARD_2['attributes']['name']}.\n",
+            f"Targets added: {BOARD_3['attributes']['name']}.\nTargets removed: {BOARD_1['attributes']['name']}.\n"
+            f"Targets modified: {BOARD_2['attributes']['name']}.\n",
             "Text is formatted correctly.",
         )
