11b10f3372b9c56666bf17d6186772d831de6288
Robert Walton <rob.walton@arm.com>
2020-11-16 17:53:27 +0000

    MbedProgram: Remove library handling
    Remove the library reference handling from the MbedProgram object.
    
    We no longer need this handling as we can just use the LibraryReferences
    class directly. We don't need to create an entire MbedProgram instance
    to fetch libraries.

--- tests/project/test_mbed_program.py
+++ tests/project/test_mbed_program.py
@@ -5,13 +5,13 @@
 import os
 import pathlib
 
-from unittest import mock, TestCase
+from unittest import TestCase
 
 from mbed_tools.project import MbedProgram
 from mbed_tools.project.exceptions import ExistingProgram, ProgramNotFound, MbedOSNotFound
 from mbed_tools.project.mbed_program import _find_program_root, parse_url
 from mbed_tools.project._internal.project_data import MbedProgramFiles
-from tests.project.factories import make_mbed_program_files, make_mbed_os_files, make_mbed_lib_reference, patchfs
+from tests.project.factories import make_mbed_program_files, make_mbed_os_files, patchfs
 
 
 class TestInitialiseProgram(TestCase):
@@ -61,28 +61,6 @@ class TestInitialiseProgram(TestCase):
         self.assertEqual(program.files, MbedProgramFiles.from_existing(program_root))
 
     @patchfs
-    @mock.patch("mbed_tools.project.mbed_program.git_utils.clone", autospec=True)
-    def test_from_url_returns_valid_program(self, mock_clone, fs):
-        fs_root = pathlib.Path(fs, "foo")
-        url = "https://valid"
-        mock_clone.side_effect = lambda *args: make_mbed_program_files(fs_root)
-        program = MbedProgram.from_url(url, fs_root)
-
-        self.assertEqual(program.files, MbedProgramFiles.from_existing(fs_root))
-        mock_clone.assert_called_once_with(url, fs_root)
-
-    @patchfs
-    @mock.patch("mbed_tools.project.mbed_program.git_utils.clone", autospec=True)
-    def test_from_url_does_not_raise_with_odd_program_layout(self, mock_clone, fs):
-        fs_root = pathlib.Path(fs, "foo")
-        fs_root.mkdir()
-        url = "https://nested"
-        mock_clone.side_effect = lambda *args: make_mbed_program_files(fs_root / "nested")
-        MbedProgram.from_url(url, fs_root)
-
-        mock_clone.assert_called_once_with(url, fs_root)
-
-    @patchfs
     def test_from_existing_raises_if_path_is_not_a_program(self, fs):
         fs_root = pathlib.Path(fs, "foo")
         fs_root.mkdir()
@@ -109,52 +87,6 @@ class TestInitialiseProgram(TestCase):
 
         self.assertTrue(program.files.app_config_file.exists())
         self.assertTrue(program.mbed_os.root.exists())
-
-
-class TestLibReferenceHandling(TestCase):
-    @mock.patch("mbed_tools.project.mbed_program.LibraryReferences", autospec=True)
-    @mock.patch("mbed_tools.project.mbed_program.MbedProgramFiles")
-    @mock.patch("mbed_tools.project.mbed_program.MbedOS")
-    def test_resolve_libraries_delegation(self, mbed_os, mbed_program_files, mock_lib_refs):
-        program = MbedProgram(mbed_program_files(), mbed_os())
-        program.resolve_libraries()
-
-        program.lib_references.fetch.assert_called_once()
-
-    @mock.patch("mbed_tools.project.mbed_program.LibraryReferences", autospec=True)
-    @mock.patch("mbed_tools.project.mbed_program.MbedProgramFiles")
-    @mock.patch("mbed_tools.project.mbed_program.MbedOS")
-    def test_deploy_libraries_delegation(self, mbed_os, mbed_program_files, mock_lib_refs):
-        program = MbedProgram(mbed_program_files(), mbed_os())
-        program.deploy_libraries()
-
-        program.lib_references.checkout.assert_called_once()
-
-    @patchfs
-    def test_lists_all_known_libraries(self, fs):
-        root = pathlib.Path(fs, "root").absolute().resolve()
-        make_mbed_program_files(root)
-        lib_ref = make_mbed_lib_reference(root, resolved=True, ref_url="https://blah")
-        lib_ref_unresolved = make_mbed_lib_reference(
-            root, name="my-unresolved-lib.lib", resolved=False, ref_url="https://blah"
-        )
-
-        program = MbedProgram.from_existing(root, check_mbed_os=False)
-        libs = program.list_known_library_dependencies()
-
-        self.assertEqual(str(lib_ref_unresolved), str(libs[1]))
-        self.assertEqual(str(lib_ref), str(libs[2]))
-
-    @patchfs
-    def test_checks_for_unresolved_libraries(self, fs):
-        root = pathlib.Path(fs, "root").absolute().resolve()
-        make_mbed_program_files(root)
-        make_mbed_lib_reference(root, resolved=True, ref_url="https://blah")
-        make_mbed_lib_reference(root, name="my-unresolved-lib.lib", resolved=False, ref_url="https://blah")
-
-        program = MbedProgram.from_existing(root, check_mbed_os=False)
-
-        self.assertTrue(program.has_unresolved_libraries())
 
 
 class TestParseURL(TestCase):
