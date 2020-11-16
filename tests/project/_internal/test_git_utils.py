6d9d4ba88bbc9f36516ffce197384250dccec3a1
Robert Walton <rob.walton@arm.com>
2020-11-16 17:46:58 +0000

    git_utils: Remove unittest.TestCase from tests
    Refactor tests into the pytest style for cleaner use of mocks and easier
    readability.

--- tests/project/_internal/test_git_utils.py
+++ tests/project/_internal/test_git_utils.py
@@ -3,59 +3,68 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 from pathlib import Path
-from unittest import TestCase, mock
+from unittest import mock
+
+import pytest
 
 from mbed_tools.project.exceptions import VersionControlError
 from mbed_tools.project._internal import git_utils
 
 
-class TestClone(TestCase):
-    @mock.patch("mbed_tools.project._internal.git_utils.git.Repo", autospec=True)
-    @mock.patch("mbed_tools.project._internal.git_utils.ProgressReporter", autospec=True)
+@pytest.fixture
+def mock_repo():
+    with mock.patch("mbed_tools.project._internal.git_utils.git.Repo") as repo:
+        yield repo
+
+
+@pytest.fixture
+def mock_progress():
+    with mock.patch("mbed_tools.project._internal.git_utils.ProgressReporter") as progress:
+        yield progress
+
+
+class TestClone:
     def test_returns_repo(self, mock_progress, mock_repo):
         url = "https://blah"
         path = Path()
         repo = git_utils.clone(url, path)
 
-        self.assertIsNotNone(repo)
+        assert repo is not None
         mock_repo.clone_from.assert_called_once_with(url, str(path), progress=mock_progress())
 
     def test_raises_when_clone_fails(self):
-        with self.assertRaises(VersionControlError):
+        with pytest.raises(VersionControlError):
             git_utils.clone("", Path())
 
 
-@mock.patch("mbed_tools.project._internal.git_utils.git.Repo", autospec=True)
-class TestInit(TestCase):
+class TestInit:
     def test_returns_initialised_repo(self, mock_repo):
         repo = git_utils.init(Path())
 
-        self.assertIsNotNone(repo)
+        assert repo is not None
         mock_repo.init.assert_called_once_with(str(Path()))
 
     def test_raises_when_init_fails(self, mock_repo):
         mock_repo.init.side_effect = git_utils.git.exc.GitCommandError("git init", 255)
 
-        with self.assertRaises(VersionControlError):
+        with pytest.raises(VersionControlError):
             git_utils.init(Path())
 
 
-@mock.patch("mbed_tools.project._internal.git_utils.git.Repo", autospec=True)
-class TestGetRepo(TestCase):
+class TestGetRepo:
     def test_returns_repo_object(self, mock_repo):
         repo = git_utils.get_repo(Path())
 
-        self.assertTrue(isinstance(repo, mock_repo.__class__))
+        assert isinstance(repo, mock_repo().__class__)
 
     def test_raises_version_control_error_when_no_git_repo_found(self, mock_repo):
         mock_repo.side_effect = git_utils.git.exc.InvalidGitRepositoryError
 
-        with self.assertRaises(VersionControlError):
+        with pytest.raises(VersionControlError):
             git_utils.get_repo(Path())
 
 
-@mock.patch("mbed_tools.project._internal.git_utils.git.Repo", autospec=True)
-class TestCheckout(TestCase):
+class TestCheckout:
     def test_git_lib_called_with_correct_command(self, mock_repo):
         git_utils.checkout(mock_repo, "master", force=True)
 
@@ -64,5 +73,5 @@ class TestCheckout(TestCase):
     def test_raises_version_control_error_when_git_checkout_fails(self, mock_repo):
         mock_repo.git.checkout.side_effect = git_utils.git.exc.GitCommandError("git checkout", 255)
 
-        with self.assertRaises(VersionControlError):
+        with pytest.raises(VersionControlError):
             git_utils.checkout(mock_repo, "bad")
