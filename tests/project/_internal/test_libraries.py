26e35d060c9595c8d39022ac5a9182e492e893e1
Robert Walton <rob.walton@arm.com>
2020-11-17 15:08:21 +0000

    libraries: Remove unittest.TestCase from tests
    Refactor the tests into the pytest style by removing unittest.TestCase
    inheritance and creating pytest fixtures to create the mock objects used
    in the test suite. This makes the tests more readable and easier to
    change or extend in the future.

--- tests/project/_internal/test_libraries.py
+++ tests/project/_internal/test_libraries.py
@@ -4,17 +4,35 @@
 #
 import pathlib
 
-from unittest import TestCase, mock
+import pytest
+
+from unittest import mock
 
 from mbed_tools.project._internal.libraries import MbedLibReference, LibraryReferences
-from tests.project.factories import make_mbed_lib_reference, patchfs
+from tests.project.factories import make_mbed_lib_reference
 
 
-@mock.patch("mbed_tools.project._internal.git_utils.clone", autospec=True)
-class TestLibraryReferences(TestCase):
-    @patchfs
-    def test_hydrates_top_level_library_references(self, mock_clone, fs):
-        fs_root = pathlib.Path(fs, "foo")
+@pytest.fixture
+def mock_clone():
+    with mock.patch("mbed_tools.project._internal.git_utils.clone") as clone:
+        yield clone
+
+
+@pytest.fixture
+def mock_checkout():
+    with mock.patch("mbed_tools.project._internal.git_utils.checkout") as checkout:
+        yield checkout
+
+
+@pytest.fixture
+def mock_get_repo():
+    with mock.patch("mbed_tools.project._internal.git_utils.get_repo") as get_repo:
+        yield get_repo
+
+
+class TestLibraryReferences:
+    def test_hydrates_top_level_library_references(self, mock_clone, tmp_path):
+        fs_root = pathlib.Path(tmp_path, "foo")
         lib = make_mbed_lib_reference(fs_root, ref_url="https://git")
         mock_clone.side_effect = lambda url, dst_dir: dst_dir.mkdir()
 
@@ -22,11 +40,10 @@ class TestLibraryReferences(TestCase):
         lib_refs.fetch()
 
         mock_clone.assert_called_once_with(lib.get_git_reference().repo_url, lib.source_code_path)
-        self.assertTrue(lib.is_resolved())
+        assert lib.is_resolved()
 
-    @patchfs
-    def test_hydrates_recursive_dependencies(self, mock_clone, fs):
-        fs_root = pathlib.Path(fs, "foo")
+    def test_hydrates_recursive_dependencies(self, mock_clone, tmp_path):
+        fs_root = pathlib.Path(tmp_path, "foo")
         lib = make_mbed_lib_reference(fs_root, ref_url="https://git")
         # Create a lib reference without touching the fs at this point, we want to mock the effects of a recursive
         # reference lookup and we need to assert the reference was resolved.
@@ -43,14 +60,11 @@ class TestLibraryReferences(TestCase):
         lib_refs = LibraryReferences(fs_root, ignore_paths=["mbed-os"])
         lib_refs.fetch()
 
-        self.assertTrue(lib.is_resolved())
-        self.assertTrue(lib2.is_resolved())
+        assert lib.is_resolved()
+        assert lib2.is_resolved()
 
-    @patchfs
-    @mock.patch("mbed_tools.project._internal.git_utils.checkout", autospec=True)
-    @mock.patch("mbed_tools.project._internal.git_utils.init", autospec=True)
-    def test_does_not_perform_deploy_if_no_git_ref_exists(self, mock_init, mock_checkout, mock_clone, fs):
-        fs_root = pathlib.Path(fs, "foo")
+    def test_does_not_perform_checkout_if_no_git_ref_exists(self, mock_get_repo, mock_checkout, mock_clone, tmp_path):
+        fs_root = pathlib.Path(tmp_path, "foo")
         make_mbed_lib_reference(fs_root, ref_url="https://git", resolved=True)
 
         lib_refs = LibraryReferences(fs_root, ignore_paths=["mbed-os"])
@@ -58,23 +72,19 @@ class TestLibraryReferences(TestCase):
 
         mock_checkout.assert_not_called()
 
-    @patchfs
-    @mock.patch("mbed_tools.project._internal.git_utils.checkout", autospec=True)
-    @mock.patch("mbed_tools.project._internal.git_utils.init", autospec=True)
-    def test_performs_deploy_if_git_ref_exists(self, mock_init, mock_checkout, mock_clone, fs):
-        fs_root = pathlib.Path(fs, "foo")
+    def test_performs_checkout_if_git_ref_exists(self, mock_get_repo, mock_checkout, mock_clone, tmp_path):
+        fs_root = pathlib.Path(tmp_path, "foo")
         lib = make_mbed_lib_reference(fs_root, ref_url="https://git#lajdhalk234", resolved=True)
 
         lib_refs = LibraryReferences(fs_root, ignore_paths=["mbed-os"])
         lib_refs.checkout(force=False)
 
-        mock_checkout.assert_called_once_with(mock_init.return_value, lib.get_git_reference().ref, force=False)
+        mock_checkout.assert_called_once_with(mock_get_repo.return_value, lib.get_git_reference().ref, force=False)
 
-    @patchfs
-    @mock.patch("mbed_tools.project._internal.git_utils.checkout", autospec=True)
-    @mock.patch("mbed_tools.project._internal.git_utils.init", autospec=True)
-    def test_fetch_does_not_perform_checkout_if_no_git_ref_exists(self, mock_init, mock_checkout, mock_clone, fs):
-        fs_root = pathlib.Path(fs, "foo")
+    def test_fetch_does_not_perform_checkout_if_no_git_ref_exists(
+        self, mock_get_repo, mock_checkout, mock_clone, tmp_path
+    ):
+        fs_root = pathlib.Path(tmp_path, "foo")
         make_mbed_lib_reference(fs_root, ref_url="https://git")
         mock_clone.side_effect = lambda url, dst_dir: dst_dir.mkdir()
 
@@ -83,11 +93,8 @@ class TestLibraryReferences(TestCase):
 
         mock_checkout.assert_not_called()
 
-    @patchfs
-    @mock.patch("mbed_tools.project._internal.git_utils.checkout", autospec=True)
-    @mock.patch("mbed_tools.project._internal.git_utils.init", autospec=True)
-    def test_fetch_performs_checkout_if_git_ref_exists(self, mock_init, mock_checkout, mock_clone, fs):
-        fs_root = pathlib.Path(fs, "foo")
+    def test_fetch_performs_checkout_if_git_ref_exists(self, mock_get_repo, mock_checkout, mock_clone, tmp_path):
+        fs_root = pathlib.Path(tmp_path, "foo")
         lib = make_mbed_lib_reference(fs_root, ref_url="https://git#lajdhalk234")
         mock_clone.side_effect = lambda url, dst_dir: dst_dir.mkdir()
 
@@ -96,11 +103,8 @@ class TestLibraryReferences(TestCase):
 
         mock_checkout.assert_called_once_with(None, lib.get_git_reference().ref)
 
-    @patchfs
-    @mock.patch("mbed_tools.project._internal.git_utils.checkout", autospec=True)
-    @mock.patch("mbed_tools.project._internal.git_utils.init", autospec=True)
-    def test_does_not_resolve_references_in_ignore_paths(self, mock_init, mock_checkout, mock_clone, fs):
-        fs_root = pathlib.Path(fs, "mbed-os")
+    def test_does_not_resolve_references_in_ignore_paths(self, mock_get_repo, mock_checkout, mock_clone, tmp_path):
+        fs_root = pathlib.Path(tmp_path, "mbed-os")
         make_mbed_lib_reference(fs_root, ref_url="https://git#lajdhalk234")
 
         lib_refs = LibraryReferences(fs_root, ignore_paths=["mbed-os"])
