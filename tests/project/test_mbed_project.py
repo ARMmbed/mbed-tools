4390461fad1fdfec3788d215245e922bfb4ab1e2
Robert Walton <rob.walton@arm.com>
2020-11-17 15:08:37 +0000

    mbed_project: Remove unittest.TestCase from tests
    Move to the pytest style for speed, readability and easier
    maintainability.

--- tests/project/test_mbed_project.py
+++ tests/project/test_mbed_project.py
@@ -4,13 +4,26 @@
 #
 import pathlib
 
-from unittest import TestCase, mock
+import pytest
+
+from unittest import mock
 
 from mbed_tools.project import initialise_project, import_project, deploy_project, get_known_libs
 
 
-@mock.patch("mbed_tools.project.project.MbedProgram", autospec=True)
-class TestInitialiseProject(TestCase):
+@pytest.fixture
+def mock_program():
+    with mock.patch("mbed_tools.project.project.MbedProgram") as prog:
+        yield prog
+
+
+@pytest.fixture
+def mock_git():
+    with mock.patch("mbed_tools.project.project.git_utils") as gutils:
+        yield gutils
+
+
+class TestInitialiseProject:
     def test_fetches_mbed_os_when_create_only_is_false(self, mock_program):
         path = pathlib.Path()
         initialise_project(path, create_only=False)
@@ -26,8 +39,7 @@ class TestInitialiseProject(TestCase):
         mock_program.from_new.return_value.resolve_libraries.assert_not_called()
 
 
-@mock.patch("mbed_tools.project.project.MbedProgram", autospec=True)
-class TestImportProject(TestCase):
+class TestImportProject:
     def test_clones_from_remote(self, mock_program):
         url = "https://git.com/gitorg/repo"
         import_project(url, recursive=False)
@@ -42,13 +54,12 @@ class TestImportProject(TestCase):
         mock_program.from_url.return_value.resolve_libraries.assert_called_once()
 
 
-@mock.patch("mbed_tools.project.project.MbedProgram", autospec=True)
-class TestDeployProject(TestCase):
+class TestDeployProject:
     def test_checks_out_libraries(self, mock_program):
         path = pathlib.Path("somewhere")
         deploy_project(path, force=False)
 
-        mock_program.from_existing.assert_called_once_with(path, False)
+        mock_program.from_existing.assert_called_once_with(path, check_mbed_os=False)
         mock_program.from_existing.return_value.deploy_libraries.assert_called_once_with(force=False)
 
     def test_resolves_libs_if_unresolved_detected(self, mock_program):
@@ -58,11 +69,10 @@ class TestDeployProject(TestCase):
         mock_program.from_existing.return_value.resolve_libraries.assert_called_once()
 
 
-@mock.patch("mbed_tools.project.project.MbedProgram", autospec=True)
-class TestPrintLibs(TestCase):
+class TestPrintLibs:
     def test_list_libraries_called(self, mock_program):
         path = pathlib.Path("somewhere")
         get_known_libs(path)
 
-        mock_program.from_existing.assert_called_once_with(path, False)
+        mock_program.from_existing.assert_called_once_with(path, check_mbed_os=False)
         mock_program.from_existing.return_value.list_known_library_dependencies.assert_called()
