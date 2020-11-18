ae1faac76cb106dd865208094743e3fffd27cf61
Robert Walton <rob.walton@arm.com>
2020-11-18 13:16:59 +0000

    project_management: Remove unittest.TestCase from tests
    Refactor unit tests into the pytest style for easier readability and
    cleaner use of mocks.

--- tests/cli/test_project_management.py
+++ tests/cli/test_project_management.py
@@ -4,44 +4,65 @@
 #
 import pathlib
 
-from unittest import TestCase, mock
+from unittest import mock
+
+import pytest
 
 from click.testing import CliRunner
 
 from mbed_tools.cli.project_management import new, import_, deploy, libs
 
 
-@mock.patch("mbed_tools.cli.project_management.initialise_project", autospec=True)
-class TestNewCommand(TestCase):
+@pytest.fixture
+def mock_initialise_project():
+    with mock.patch("mbed_tools.cli.project_management.initialise_project") as init_proj:
+        yield init_proj
+
+
+@pytest.fixture
+def mock_import_project():
+    with mock.patch("mbed_tools.cli.project_management.import_project") as import_proj:
+        yield import_proj
+
+
+@pytest.fixture
+def mock_deploy_project():
+    with mock.patch("mbed_tools.cli.project_management.deploy_project") as deploy_proj:
+        yield deploy_proj
+
+
+@pytest.fixture
+def mock_get_libs():
+    with mock.patch("mbed_tools.cli.project_management.get_known_libs") as get_libs:
+        yield get_libs
+
+
+class TestNewCommand:
     def test_calls_new_function_with_correct_args(self, mock_initialise_project):
         CliRunner().invoke(new, ["path", "--create-only"])
         mock_initialise_project.assert_called_once_with(pathlib.Path("path"), True)
 
     def test_echos_mbed_os_message_when_required(self, mock_initialise_project):
+        expected = "Creating a new Mbed program at path 'path'.\nDownloading mbed-os and adding it to the project.\n"
+
         result = CliRunner().invoke(new, ["path"])
 
-        self.assertEqual(
-            result.output,
-            "Creating a new Mbed program at path 'path'.\nDownloading mbed-os and adding it to the project.\n",
-        )
+        assert result.output == expected
 
 
-@mock.patch("mbed_tools.cli.project_management.import_project", autospec=True)
-class TestImportCommand(TestCase):
-    def test_calls_import_function_with_correct_args(self, mocked_import_project):
+class TestImportCommand:
+    def test_calls_clone_function_with_correct_args(self, mock_import_project):
         CliRunner().invoke(import_, ["url", "dst"])
-        mocked_import_project.assert_called_once_with("url", pathlib.Path("dst"), True)
+        mock_import_project.assert_called_once_with("url", pathlib.Path("dst"), True)
 
 
-@mock.patch("mbed_tools.cli.project_management.get_known_libs", autospec=True)
-class TestLibsCommand(TestCase):
-    def test_calls_libs_function(self, mocked_get_libs):
+class TestLibsCommand:
+    def test_calls_libs_function(self, mock_get_libs):
         CliRunner().invoke(libs)
-        mocked_get_libs.assert_called_once()
+        mock_get_libs.assert_called_once()
 
 
-@mock.patch("mbed_tools.cli.project_management.deploy_project", autospec=True)
-class TestDeployCommand(TestCase):
-    def test_calls_deploy_function_with_correct_args(self, mocked_deploy_project):
+class TestDeployCommand:
+    def test_calls_deploy_function_with_correct_args(self, mock_deploy_project):
         CliRunner().invoke(deploy, ["path", "--force"])
-        mocked_deploy_project.assert_called_once_with(pathlib.Path("path"), True)
+        mock_deploy_project.assert_called_once_with(pathlib.Path("path"), True)
