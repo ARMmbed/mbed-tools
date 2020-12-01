b1de8e79d99cff72c7f8645adb196189e6d911ca
Jaeden Amero <jaeden.amero@arm.com>
2020-12-01 15:26:42 +0000

    tests: Make CMake config tests use shared fake
    Instead of having each test copy-paste assembly their own target, create
    a pytest fixture for the fake target which the tests can share.

--- tests/build/_internal/test_cmake_file.py
+++ tests/build/_internal/test_cmake_file.py
@@ -2,63 +2,58 @@
 # Copyright (C) 2020 Arm Mbed. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
+import pytest
+
 from tests.build._internal.config.factories import ConfigFactory
 from mbed_tools.build._internal.cmake_file import generate_mbed_config_cmake_file, _render_mbed_config_cmake_template
 
 
+TOOLCHAIN_NAME = "gcc"
+
+
+@pytest.fixture()
+def fake_target():
+    return {
+        "labels": ["foo"],
+        "extra_labels": ["morefoo"],
+        "features": ["bar"],
+        "components": ["baz"],
+        "macros": ["macbaz"],
+        "device_has": ["stuff"],
+        "c_lib": ["c_lib"],
+        "core": ["core"],
+        "printf_lib": ["printf_lib"],
+        "supported_form_factors": ["arduino"],
+        "supported_c_libs": {TOOLCHAIN_NAME: ["ginormous"]},
+        "supported_application_profiles": ["full", "bare-metal"],
+    }
+
+
 class TestGenerateCMakeListsFile:
-    def test_correct_arguments_passed(self):
-        target = dict()
-        target["labels"] = ["foo"]
-        target["extra_labels"] = ["morefoo"]
-        target["features"] = ["bar"]
-        target["components"] = ["baz"]
-        target["macros"] = ["macbaz"]
-        target["device_has"] = ["stuff"]
-        target["c_lib"] = ["c_lib"]
-        target["core"] = ["core"]
-        target["printf_lib"] = ["printf_lib"]
-        target["supported_form_factors"] = ["arduino"]
+    def test_correct_arguments_passed(self, fake_target):
         config = ConfigFactory()
         mbed_target = "K64F"
-        toolchain_name = "GCC"
-        target["supported_c_libs"] = {toolchain_name.lower(): ["small", "std"]}
-        target["supported_application_profiles"] = ["full", "bare-metal"]
 
-        result = generate_mbed_config_cmake_file(mbed_target, target, config, toolchain_name)
+        result = generate_mbed_config_cmake_file(mbed_target, fake_target, config, TOOLCHAIN_NAME)
 
-        assert result == _render_mbed_config_cmake_template(target, config, toolchain_name, mbed_target,)
+        assert result == _render_mbed_config_cmake_template(fake_target, config, TOOLCHAIN_NAME, mbed_target,)
 
 
 class TestRendersCMakeListsFile:
-    def test_returns_rendered_content(self):
-        target = dict()
-        target["labels"] = ["foo"]
-        target["extra_labels"] = ["morefoo"]
-        target["features"] = ["bar"]
-        target["components"] = ["baz"]
-        target["macros"] = ["macbaz"]
-        target["device_has"] = ["stuff"]
-        target["core"] = ["core"]
-        target["c_lib"] = ["c_lib"]
-        target["printf_lib"] = ["printf_lib"]
-        target["supported_form_factors"] = ["arduino"]
+    def test_returns_rendered_content(self, fake_target):
         config = ConfigFactory()
-        toolchain_name = "baz"
-        target["supported_c_libs"] = {toolchain_name.lower(): ["small", "std"]}
-        target["supported_application_profiles"] = ["full", "bare-metal"]
-        result = _render_mbed_config_cmake_template(target, config, toolchain_name, "target_name")
+        result = _render_mbed_config_cmake_template(fake_target, config, TOOLCHAIN_NAME, "target_name")
 
-        for label in target["labels"] + target["extra_labels"]:
+        for label in fake_target["labels"] + fake_target["extra_labels"]:
             assert label in result
 
-        for macro in target["features"] + target["components"] + [toolchain_name]:
+        for macro in fake_target["features"] + fake_target["components"] + [TOOLCHAIN_NAME]:
             assert macro in result
 
-        for toolchain in target["supported_c_libs"]:
+        for toolchain in fake_target["supported_c_libs"]:
             assert toolchain in result
             for supported_c_libs in toolchain:
                 assert supported_c_libs in result
 
-        for supported_application_profiles in target["supported_application_profiles"]:
+        for supported_application_profiles in fake_target["supported_application_profiles"]:
             assert supported_application_profiles in result
