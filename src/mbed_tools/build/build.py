0f92102d5b589a5f3183db847449f461b4fbff2d
Robert Walton <rob.walton@arm.com>
2020-12-10 12:00:01 +0000

    build: Add checks for CMake and Ninja installs
    If CMake or Ninja is not found on PATH handle the builtin exception and
    emit a more useful error message.
    
    Fixes #149

--- src/mbed_tools/build/build.py
+++ src/mbed_tools/build/build.py
@@ -22,6 +22,7 @@ def build_project(build_dir: pathlib.Path, target: Optional[str] = None) -> None
         build_dir: Path to the CMake build tree.
         target: The CMake target to build (e.g 'install')
     """
+    _check_ninja_found()
     target_flag = ["--target", target] if target is not None else []
     _cmake_wrapper("--build", str(build_dir), *target_flag)
 
@@ -34,6 +35,7 @@ def generate_build_system(source_dir: pathlib.Path, build_dir: pathlib.Path, pro
         build_dir: Path to the CMake build tree.
         profile: The Mbed build profile (develop, debug or release).
     """
+    _check_ninja_found()
     _cmake_wrapper("-S", str(source_dir), "-B", str(build_dir), "-GNinja", f"-DCMAKE_BUILD_TYPE={profile}")
 
 
@@ -41,5 +43,16 @@ def _cmake_wrapper(*cmake_args: str) -> None:
     try:
         logger.debug("Running CMake with args: %s", cmake_args)
         subprocess.run(["cmake", *cmake_args], check=True)
+    except FileNotFoundError:
+        raise MbedBuildError("Could not find CMake. Please ensure CMake is installed and added to PATH.")
     except subprocess.CalledProcessError:
         raise MbedBuildError("CMake invocation failed!")
+
+
+def _check_ninja_found() -> None:
+    try:
+        subprocess.run(["ninja", "--version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except FileNotFoundError:
+        raise MbedBuildError(
+            "Could not find the 'Ninja' build program. Please ensure 'Ninja' is installed and added to PATH."
+        )
