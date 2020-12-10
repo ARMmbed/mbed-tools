334008d15428edcf4b8f0d9dca90f33d4fce83c8
Robert Walton <rob.walton@arm.com>
2020-12-10 12:51:59 +0000

    build: Reduce dependency on implementation details in tests
    Move the mocking closer to the "edges" of the code by just mocking the
    subprocess.run function, as we are always going to use this library to
    trigger the CMake subprocess. This should make the tests more resistant
    to changes in the helper functions used to wrap subprocess in build.py.

--- tests/build/test_build.py
+++ tests/build/test_build.py
@@ -13,27 +13,21 @@ from mbed_tools.build.exceptions import MbedBuildError
 
 
 @pytest.fixture
-def cmake_wrapper():
-    with mock.patch("mbed_tools.build.build._cmake_wrapper") as cmake:
-        yield cmake
-
-
-@pytest.fixture
 def subprocess_run():
     with mock.patch("mbed_tools.build.build.subprocess.run", autospec=True) as subproc:
         yield subproc
 
 
 class TestBuildProject:
-    def test_invokes_cmake_with_correct_args(self, cmake_wrapper):
+    def test_invokes_cmake_with_correct_args(self, subprocess_run):
         build_project(build_dir="cmake_build", target="install")
 
-        cmake_wrapper.assert_called_once_with("--build", "cmake_build", "--target", "install")
+        subprocess_run.assert_called_with(["cmake", "--build", "cmake_build", "--target", "install"], check=True)
 
-    def test_invokes_cmake_with_correct_args_if_no_target_passed(self, cmake_wrapper):
+    def test_invokes_cmake_with_correct_args_if_no_target_passed(self, subprocess_run):
         build_project(build_dir="cmake_build")
 
-        cmake_wrapper.assert_called_once_with("--build", "cmake_build")
+        subprocess_run.assert_called_with(["cmake", "--build", "cmake_build"], check=True)
 
     def test_raises_build_error_if_cmake_invocation_fails(self, subprocess_run):
         subprocess_run.side_effect = subprocess.CalledProcessError(1, "")
@@ -43,13 +37,13 @@ class TestBuildProject:
 
 
 class TestConfigureProject:
-    def test_invokes_cmake_with_correct_args(self, cmake_wrapper):
+    def test_invokes_cmake_with_correct_args(self, subprocess_run):
         source_dir = "source_dir"
         build_dir = "cmake_build"
         profile = "debug"
 
         generate_build_system(source_dir, build_dir, profile)
 
-        cmake_wrapper.assert_called_once_with(
-            "-S", source_dir, "-B", build_dir, "-GNinja", f"-DCMAKE_BUILD_TYPE={profile}"
+        subprocess_run.assert_called_with(
+            ["cmake", "-S", source_dir, "-B", build_dir, "-GNinja", f"-DCMAKE_BUILD_TYPE={profile}"], check=True
         )
