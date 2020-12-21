18f667f2102bc48d2379a6c0da55a98fe13b3bf5
Robert Walton <rob.walton@arm.com>
2020-12-21 16:43:48 +0000

    cmake_file: Remove unnecessary function

--- src/mbed_tools/build/_internal/cmake_file.py
+++ src/mbed_tools/build/_internal/cmake_file.py
@@ -13,22 +13,8 @@ TEMPLATES_DIRECTORY = pathlib.Path("_internal", "templates")
 TEMPLATE_NAME = "mbed_config.tmpl"
 
 
-def generate_mbed_config_cmake_file(mbed_target_name: str, config: Config, toolchain_name: str) -> str:
-    """Generate mbed_config.cmake containing the correct definitions for a build.
-
-    Args:
-        mbed_target_name: the target the application is being built for
-        config: Config object holding information parsed from the mbed config system.
-        toolchain_name: the toolchain to be used to build the application
-
-    Returns:
-        A string of rendered contents for the file.
-    """
-    return _render_mbed_config_cmake_template(config, toolchain_name, mbed_target_name,)
-
-
-def _render_mbed_config_cmake_template(config: Config, toolchain_name: str, target_name: str) -> str:
-    """Renders the mbed_config template with the relevant information.
+def render_mbed_config_cmake_template(config: Config, toolchain_name: str, target_name: str) -> str:
+    """Renders the mbed_config jinja template with the target and project config settings.
 
     Args:
         config: Config object holding information parsed from the mbed config system.
@@ -36,7 +22,7 @@ def _render_mbed_config_cmake_template(config: Config, toolchain_name: str, targ
         target_name: Name of the target.
 
     Returns:
-        The contents of the rendered CMake file.
+        The rendered mbed_config template.
     """
     env = jinja2.Environment(loader=jinja2.PackageLoader("mbed_tools.build", str(TEMPLATES_DIRECTORY)),)
     template = env.get_template(TEMPLATE_NAME)
