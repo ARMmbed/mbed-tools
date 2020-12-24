1306170d5f4a0e05e345b49754b2348ae0a078d6
Robert Walton <rob.walton@arm.com>
2020-12-24 10:05:22 +0000

    lib: Add debug logging to json_helpers.decode_json

--- src/mbed_tools/lib/json_helpers.py
+++ src/mbed_tools/lib/json_helpers.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 def decode_json_file(path: Path) -> Any:
     """Return the contents of json file."""
     try:
+        logger.debug(f"Loading JSON file {path}")
         return json.loads(path.read_text())
     except json.JSONDecodeError:
         logger.error(f"Failed to decode JSON data in the file located at '{path}'")
