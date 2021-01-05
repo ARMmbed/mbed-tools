28751d8fa010e5901eff47bab13938f6d25fa0db
Robert Walton <rob.walton@arm.com>
2021-01-05 11:37:09 +0000

    Update copyright notice in source files
    The copyright notice in our source was incorrect. The organisation
    should be "Arm Limited", not "Arm Mbed".
    
    Update the copyright notice so we're compliant with Arm OSS
    requirements.

--- src/mbed_tools/build/__init__.py
+++ src/mbed_tools/build/__init__.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 Arm Mbed. All rights reserved.
+# Copyright (c) 2020-2021 Arm Limited and Contributors. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 """Provides the core build system for Mbed OS, which relies on CMake and Ninja as underlying technologies.
--- tests/targets/test_config.py
+++ tests/targets/test_config.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 Arm Mbed. All rights reserved.
+# Copyright (c) 2020-2021 Arm Limited and Contributors. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 import os
