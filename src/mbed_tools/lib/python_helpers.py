5bcd8219efdada1178ebd20925cd5d52f78d62d9
Robert Walton <rob.walton@arm.com>
2021-02-17 14:26:48 +0000

    python_helpers: Add a function to flatten nested lists

--- src/mbed_tools/lib/python_helpers.py
+++ src/mbed_tools/lib/python_helpers.py
@@ -5,7 +5,7 @@
 """Helpers for python language related functions."""
 import sys
 from collections import namedtuple
-from typing import NamedTuple, Iterable, Any, cast
+from typing import NamedTuple, Iterable, Any, List, cast
 
 
 def minimum_python_version(major: int, minor: int) -> bool:
@@ -25,3 +25,25 @@ def named_tuple_with_defaults(typename: str, field_names: Iterable[str], default
         a_tuple: NamedTuple = namedtuple(typename=typename, field_names=field_names)  # type: ignore
         a_tuple.__new__.__defaults__ = tuple(defaults)  # type: ignore
         return cast(NamedTuple, a_tuple)
+
+
+def flatten_nested(input_iter: Iterable) -> List:
+    """Flatten a nested Iterable with arbitrary levels of nesting.
+
+    If the input is an iterator then this function will exhaust it.
+
+    Args:
+        input_iter: The input Iterable which may or may not be nested.
+
+    Returns:
+        A flat list created from the input_iter.
+        If input_iter has no nesting its elements are appended to a list and returned.
+    """
+    output = []
+    for elem in input_iter:
+        if isinstance(elem, Iterable) and not isinstance(elem, str):
+            output += flatten_nested(elem)
+        else:
+            output.append(elem)
+
+    return output
