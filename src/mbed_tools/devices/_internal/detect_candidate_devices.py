f707fe0315503def1f1a1e15f48deab07c3959d4
Robert Walton <rob.walton@arm.com>
2021-02-18 11:17:40 +0000

    detect: Raise an error when running on unknown OS
    Previously the detection code seemed to think if the user wasn't running
    Linux or Windows they must be running macOS. That would be great, but
    unfortunately this isn't the case and other OSes do exist.
    
    This commit adds an UnknownOSError and raises it when platform.system
    detects an OS we don't understand.
    
    Fixes #177

--- src/mbed_tools/devices/_internal/detect_candidate_devices.py
+++ src/mbed_tools/devices/_internal/detect_candidate_devices.py
@@ -8,6 +8,7 @@ from typing import Iterable
 
 from mbed_tools.devices._internal.candidate_device import CandidateDevice
 from mbed_tools.devices._internal.base_detector import DeviceDetector
+from mbed_tools.devices.exceptions import UnknownOSError
 
 
 def detect_candidate_devices() -> Iterable[CandidateDevice]:
@@ -26,7 +27,12 @@ def _get_detector_for_current_os() -> DeviceDetector:
         from mbed_tools.devices._internal.linux.device_detector import LinuxDeviceDetector
 
         return LinuxDeviceDetector()
-    else:
+    if platform.system() == "Darwin":
         from mbed_tools.devices._internal.darwin.device_detector import DarwinDeviceDetector
 
         return DarwinDeviceDetector()
+
+    raise UnknownOSError(
+        f"We have detected the OS you are running is '{platform.system()}'. "
+        "Unfortunately we haven't implemented device detection support for this OS yet. Sorry!"
+    )
