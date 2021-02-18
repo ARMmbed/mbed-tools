6cd30a7d510e2b6a158578c802d28479e0fa7124
Werner Lewis <Werner.Lewis@arm.com>
2021-02-18 16:04:43 +0000

    detect: Add ID for connected devices of same type
    When multiple devices of the same type are connected, they are listed
    in the detect subcommand with non-unique build targets.
    
    Adds unique build targets when multiple of the same board are connected
    by including the device's ID in the format TARGET[ID]. This can then be
    used in compile and sterm subcommands.

--- tests/cli/test_list_connected_devices.py
+++ tests/cli/test_list_connected_devices.py
@@ -153,8 +153,48 @@ class TestBuildTableOutput(TestCase):
                     device.serial_number,
                     device.serial_port,
                     "\n".join(map(str, device.mount_points)),
-                    "\n".join(_get_build_targets(device.mbed_board)),
+                    "\n".join(_get_build_targets(device.mbed_board, None)),
                 ]
+            ],
+            headers=["Board name", "Serial number", "Serial port", "Mount point(s)", "Build target(s)"],
+        )
+        self.assertEqual(output, expected_output)
+
+    def test_returns_tabularised_with_identifier(self):
+        board = mock.create_autospec(
+            Board, board_name="board-name", build_variant=("S", "NS"), board_type="board-type",
+        )
+        device = Device(
+            mbed_board=board,
+            serial_number="123",
+            serial_port="serial-port",
+            mount_points=[pathlib.Path("/Volumes/FOO"), pathlib.Path("/Volumes/BAR")],
+        )
+        device_2 = Device(
+            mbed_board=board,
+            serial_number="456",
+            serial_port="serial-port",
+            mount_points=[pathlib.Path("/Volumes/FOO"), pathlib.Path("/Volumes/BAR")],
+        )
+
+        output = _build_tabular_output([device, device_2])
+
+        expected_output = tabulate(
+            [
+                [
+                    f"{device.mbed_board.board_name}",
+                    device.serial_number,
+                    device.serial_port,
+                    "\n".join(map(str, device.mount_points)),
+                    "\n".join(_get_build_targets(board, 0)),
+                ],
+                [
+                    f"{device_2.mbed_board.board_name}",
+                    device_2.serial_number,
+                    device_2.serial_port,
+                    "\n".join(map(str, device_2.mount_points)),
+                    "\n".join(_get_build_targets(board, 1)),
+                ],
             ],
             headers=["Board name", "Serial number", "Serial port", "Mount point(s)", "Build target(s)"],
         )
@@ -177,7 +217,7 @@ class TestBuildTableOutput(TestCase):
                     device.serial_number,
                     "<unknown>",
                     "\n".join(map(str, device.mount_points)),
-                    "\n".join(_get_build_targets(device.mbed_board)),
+                    "\n".join(_get_build_targets(device.mbed_board, None)),
                 ]
             ],
             headers=["Board name", "Serial number", "Serial port", "Mount point(s)", "Build target(s)"],
@@ -213,9 +253,61 @@ class TestBuildJsonOutput(TestCase):
                         "board_name": board.board_name,
                         "mbed_os_support": board.mbed_os_support,
                         "mbed_enabled": board.mbed_enabled,
-                        "build_targets": _get_build_targets(board),
+                        "build_targets": _get_build_targets(board, None),
                     },
                 }
+            ],
+            indent=4,
+        )
+
+        self.assertEqual(output, expected_output)
+
+    def test_returns_json_representation_with_identifier(self):
+        board = mock.create_autospec(
+            Board,
+            product_code="0021",
+            board_type="HAT-BOAT",
+            board_name="HAT Boat",
+            mbed_os_support=["0.2"],
+            mbed_enabled=["potentially"],
+            build_variant=("S", "NS"),
+        )
+        device = Device(
+            mbed_board=board, serial_number="09887654", serial_port="COM1", mount_points=[pathlib.Path("somepath")],
+        )
+        device_2 = Device(
+            mbed_board=board, serial_number="09884321", serial_port="COM2", mount_points=[pathlib.Path("anotherpath")],
+        )
+
+        output = _build_json_output([device, device_2])
+        expected_output = json.dumps(
+            [
+                {
+                    "serial_number": device.serial_number,
+                    "serial_port": device.serial_port,
+                    "mount_points": [str(m) for m in device.mount_points],
+                    "mbed_board": {
+                        "product_code": board.product_code,
+                        "board_type": board.board_type,
+                        "board_name": board.board_name,
+                        "mbed_os_support": board.mbed_os_support,
+                        "mbed_enabled": board.mbed_enabled,
+                        "build_targets": _get_build_targets(board, 0),
+                    },
+                },
+                {
+                    "serial_number": device_2.serial_number,
+                    "serial_port": device_2.serial_port,
+                    "mount_points": [str(m) for m in device_2.mount_points],
+                    "mbed_board": {
+                        "product_code": board.product_code,
+                        "board_type": board.board_type,
+                        "board_name": board.board_name,
+                        "mbed_os_support": board.mbed_os_support,
+                        "mbed_enabled": board.mbed_enabled,
+                        "build_targets": _get_build_targets(board, 1),
+                    },
+                },
             ],
             indent=4,
         )
@@ -237,4 +329,8 @@ class TestGetBuildTargets(TestCase):
     def test_returns_base_target_and_all_variants(self):
         board = mock.create_autospec(Board, build_variant=("S", "NS"), board_type="FOO")
 
-        self.assertEqual(_get_build_targets(board), ["FOO_S", "FOO_NS", "FOO"])
+        self.assertEqual(_get_build_targets(board, None), ["FOO_S", "FOO_NS", "FOO"])
+
+    def test_returns_base_and_variants_identifier(self):
+        board = mock.create_autospec(Board, build_variant=("S", "NS"), board_type="FOO")
+        self.assertEqual(_get_build_targets(board, 2), ["FOO_S[2]", "FOO_NS[2]", "FOO[2]"])
