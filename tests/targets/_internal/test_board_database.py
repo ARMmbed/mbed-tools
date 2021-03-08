3db3d35e27375033ae861e13dbacc5b048035b45
Werner Lewis <Werner.Lewis@arm.com>
2021-03-08 11:18:20 +0000

    tests: Remove unittest from test_board_database

--- tests/targets/_internal/test_board_database.py
+++ tests/targets/_internal/test_board_database.py
@@ -4,71 +4,75 @@
 #
 """Tests for `mbed_tools.targets._internal.board_database`."""
 
-from unittest import TestCase, mock
+from unittest import mock
 
-import requests_mock
+import logging
+import pytest
 
-# Unit under test
 import mbed_tools.targets._internal.board_database as board_database
 from mbed_tools.targets.env import env
 
 
-class TestGetOnlineBoardData(TestCase):
+class TestGetOnlineBoardData:
     """Tests for the method `board_database.get_online_board_data`."""
 
-    @requests_mock.mock()
-    @mock.patch("mbed_tools.targets._internal.board_database.logger.warning", autospec=True)
-    @mock.patch("mbed_tools.targets._internal.board_database.logger.debug", autospec=True)
-    def test_401(self, mock_request, logger_debug, logger_warning):
+    def test_401(self, caplog, requests_mock):
         """Given a 401 error code, BoardAPIError is raised."""
-        mock_request.get(board_database._BOARD_API, status_code=401, text="Who are you?")
-        with self.assertRaises(board_database.BoardAPIError):
+        caplog.set_level(logging.DEBUG)
+        requests_mock.get(board_database._BOARD_API, status_code=401, text="Who are you?")
+        with pytest.raises(board_database.BoardAPIError):
             board_database.get_online_board_data()
-        self.assertTrue("MBED_API_AUTH_TOKEN" in str(logger_warning.call_args), "Auth token should be mentioned")
-        self.assertTrue("Who are you?" in str(logger_debug.call_args), "Message content should be in the debug message")
+        assert any(
+            x for x in caplog.records if x.levelno == logging.WARNING and "MBED_API_AUTH_TOKEN" in x.msg
+        ), "Auth token should be mentioned"
+        assert any(
+            x for x in caplog.records if x.levelno == logging.DEBUG and "Who are you?" in x.msg
+        ), "Message content should be in the debug message"
 
-    @requests_mock.mock()
-    @mock.patch("mbed_tools.targets._internal.board_database.logger.warning", autospec=True)
-    @mock.patch("mbed_tools.targets._internal.board_database.logger.debug", autospec=True)
-    def test_404(self, mock_request, logger_debug, logger_warning):
+    def test_404(self, caplog, requests_mock):
         """Given a 404 error code, TargetAPIError is raised."""
-        mock_request.get(board_database._BOARD_API, status_code=404, text="Not Found")
-        with self.assertRaises(board_database.BoardAPIError):
+        caplog.set_level(logging.DEBUG)
+        requests_mock.get(board_database._BOARD_API, status_code=404, text="Not Found")
+        with pytest.raises(board_database.BoardAPIError):
             board_database.get_online_board_data()
-        self.assertTrue("404" in str(logger_warning.call_args), "HTTP status code should be mentioned")
-        self.assertTrue("Not Found" in str(logger_debug.call_args), "Message content should be in the debug message")
+        assert any(
+            x for x in caplog.records if x.levelno == logging.WARNING and "404" in x.msg
+        ), "HTTP status code should be mentioned"
+        assert any(
+            x for x in caplog.records if x.levelno == logging.DEBUG and "Not Found" in x.msg
+        ), "Message content should be in the debug message"
 
-    @requests_mock.mock()
-    @mock.patch("mbed_tools.targets._internal.board_database.logger.warning", autospec=True)
-    @mock.patch("mbed_tools.targets._internal.board_database.logger.debug", autospec=True)
-    def test_200_invalid_json(self, mock_request, logger_debug, logger_warning):
+    def test_200_invalid_json(self, caplog, requests_mock):
         """Given a valid response but invalid json, JSONDecodeError is raised."""
-        mock_request.get(board_database._BOARD_API, text="some text")
-        with self.assertRaises(board_database.ResponseJSONError):
+        caplog.set_level(logging.DEBUG)
+        requests_mock.get(board_database._BOARD_API, text="some text")
+        with pytest.raises(board_database.ResponseJSONError):
             board_database.get_online_board_data()
-        self.assertTrue("Invalid JSON" in str(logger_warning.call_args), "Invalid JSON should be mentioned")
-        self.assertTrue("some text" in str(logger_debug.call_args), "Message content should be in the debug message")
+        assert any(
+            x for x in caplog.records if x.levelno == logging.WARNING and "Invalid JSON" in x.msg
+        ), "Invalid JSON should be mentioned"
+        assert any(
+            x for x in caplog.records if x.levelno == logging.DEBUG and "some text" in x.msg
+        ), "Message content should be in the debug message"
 
-    @requests_mock.mock()
-    @mock.patch("mbed_tools.targets._internal.board_database.logger.warning", autospec=True)
-    @mock.patch("mbed_tools.targets._internal.board_database.logger.debug", autospec=True)
-    def test_200_no_data_field(self, mock_request, logger_debug, logger_warning):
+    def test_200_no_data_field(self, caplog, requests_mock):
         """Given a valid response but no data field, ResponseJSONError is raised."""
-        mock_request.get(board_database._BOARD_API, json={"notdata": [], "stillnotdata": {}})
-        with self.assertRaises(board_database.ResponseJSONError):
+        caplog.set_level(logging.DEBUG)
+        requests_mock.get(board_database._BOARD_API, json={"notdata": [], "stillnotdata": {}})
+        with pytest.raises(board_database.ResponseJSONError):
             board_database.get_online_board_data()
-        self.assertTrue("missing the 'data' field" in str(logger_warning.call_args), "Data field should be mentioned")
-        self.assertTrue(
-            "notdata, stillnotdata" in str(logger_debug.call_args),
-            "JSON keys from message should be in the debug message",
-        )
+        assert any(
+            x for x in caplog.records if x.levelno == logging.WARNING and "missing the 'data' field" in x.msg
+        ), "Data field should be mentioned"
+        assert any(
+            x for x in caplog.records if x.levelno == logging.DEBUG and "notdata, stillnotdata" in x.msg
+        ), "JSON keys from message should be in the debug message"
 
-    @requests_mock.mock()
-    def test_200_value_data(self, mock_request):
+    def test_200_value_data(self, requests_mock):
         """Given a valid response, target data is set from the returned json."""
-        mock_request.get(board_database._BOARD_API, json={"data": 42})
+        requests_mock.get(board_database._BOARD_API, json={"data": 42})
         board_data = board_database.get_online_board_data()
-        self.assertEqual(42, board_data, "Target data should match the contents of the target API data")
+        assert 42 == board_data, "Target data should match the contents of the target API data"
 
     @mock.patch("mbed_tools.targets._internal.board_database.requests")
     @mock.patch("mbed_tools.targets._internal.board_database.env", spec_set=env)
@@ -88,33 +92,31 @@ class TestGetOnlineBoardData(TestCase):
     @mock.patch("mbed_tools.targets._internal.board_database.requests.get")
     def test_raises_tools_error_on_connection_error(self, get):
         get.side_effect = board_database.requests.exceptions.ConnectionError
-        with self.assertRaises(board_database.BoardAPIError):
+        with pytest.raises(board_database.BoardAPIError):
             board_database._get_request()
 
-    @mock.patch("mbed_tools.targets._internal.board_database.logger.warning", autospec=True)
     @mock.patch("mbed_tools.targets._internal.board_database.requests.get")
-    def test_logs_error_on_requests_ssl_error(self, get, logger_warning):
+    def test_logs_error_on_requests_ssl_error(self, get, caplog):
         get.side_effect = board_database.requests.exceptions.SSLError
-        with self.assertRaises(board_database.BoardAPIError):
+        with pytest.raises(board_database.BoardAPIError):
             board_database._get_request()
-        self.assertTrue("verify an SSL" in str(logger_warning.call_args_list))
+        assert "verify an SSL" in caplog.text
 
-    @mock.patch("mbed_tools.targets._internal.board_database.logger.warning", autospec=True)
     @mock.patch("mbed_tools.targets._internal.board_database.requests.get")
-    def test_logs_error_on_requests_proxy_error(self, get, logger_warning):
+    def test_logs_error_on_requests_proxy_error(self, get, caplog):
         get.side_effect = board_database.requests.exceptions.ProxyError
-        with self.assertRaises(board_database.BoardAPIError):
+        with pytest.raises(board_database.BoardAPIError):
             board_database._get_request()
-        self.assertTrue("connect to proxy" in str(logger_warning.call_args_list))
+        assert "connect to proxy" in caplog.text
 
 
-class TestGetOfflineTargetData(TestCase):
+class TestGetOfflineTargetData:
     """Tests for the method get_offline_target_data."""
 
     def test_local_target_database_file_found(self):
         """Test local database is found and loaded."""
         data = board_database.get_offline_board_data()
-        self.assertTrue(len(data), "Some data should be returned from local database file.")
+        assert len(data), "Some data should be returned from local database file."
 
     @mock.patch("mbed_tools.targets._internal.board_database.get_board_database_path")
     def test_raises_on_invalid_json(self, mocked_get_file):
@@ -123,11 +125,11 @@ class TestGetOfflineTargetData(TestCase):
         path_mock = mock.Mock()
         path_mock.read_text.return_value = invalid_json
         mocked_get_file.return_value = path_mock
-        with self.assertRaises(board_database.ResponseJSONError):
+        with pytest.raises(board_database.ResponseJSONError):
             board_database.get_offline_board_data()
 
 
-class TestGetLocalTargetDatabaseFile(TestCase):
+class TestGetLocalTargetDatabaseFile:
     def test_returns_path_to_targets(self):
         path = board_database.get_board_database_path()
-        self.assertEqual(path.exists(), True, "Path to boards should exist in the package data folder.")
+        assert path.exists(), "Path to boards should exist in the package data folder."
