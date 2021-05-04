b57217a8b02c2c59654211b07b552fc83d1c0120
Werner Lewis <Werner.Lewis@arm.com>
2021-05-04 12:00:13 +0100

    tests: Remove TestCase from test_mbed_program
    This removes usage of unittest.TestCase. The pytest tmp_path fixture
    is used to create temporary directories in tests rather than patchfs.

--- tests/project/test_mbed_program.py
+++ tests/project/test_mbed_program.py
@@ -4,14 +4,14 @@
 #
 import os
 import pathlib
+import pytest
 
-from unittest import TestCase
 
 from mbed_tools.project import MbedProgram
 from mbed_tools.project.exceptions import ExistingProgram, ProgramNotFound, MbedOSNotFound
 from mbed_tools.project.mbed_program import _find_program_root, parse_url
 from mbed_tools.project._internal.project_data import MbedProgramFiles
-from tests.project.factories import make_mbed_program_files, make_mbed_os_files, patchfs
+from tests.project.factories import make_mbed_program_files, make_mbed_os_files
 
 
 DEFAULT_BUILD_SUBDIR = pathlib.Path("K64F", "develop", "GCC_ARM")
@@ -24,83 +24,75 @@ def from_new_set_target_toolchain(program_root):
     return program
 
 
-class TestInitialiseProgram(TestCase):
-    @patchfs
-    def test_from_new_local_dir_raises_if_path_is_existing_program(self, fs):
-        program_root = pathlib.Path(fs, "programfoo")
+class TestInitialiseProgram:
+    def test_from_new_local_dir_raises_if_path_is_existing_program(self, tmp_path):
+        program_root = pathlib.Path(tmp_path, "programfoo")
         program_root.mkdir()
         (program_root / "mbed-os.lib").touch()
 
-        with self.assertRaises(ExistingProgram):
+        with pytest.raises(ExistingProgram):
             MbedProgram.from_new(program_root)
 
-    @patchfs
-    def test_from_new_local_dir_generates_valid_program_creating_directory(self, fs):
-        fs_root = pathlib.Path(fs, "foo")
+    def test_from_new_local_dir_generates_valid_program_creating_directory(self, tmp_path):
+        fs_root = pathlib.Path(tmp_path, "foo")
         fs_root.mkdir()
         program_root = fs_root / "programfoo"
 
         program = from_new_set_target_toolchain(program_root)
 
-        self.assertEqual(program.files, MbedProgramFiles.from_existing(program_root, DEFAULT_BUILD_SUBDIR))
+        assert program.files == MbedProgramFiles.from_existing(program_root, DEFAULT_BUILD_SUBDIR)
 
-    @patchfs
-    def test_from_new_local_dir_generates_valid_program_creating_directory_in_cwd(self, fs):
+    def test_from_new_local_dir_generates_valid_program_creating_directory_in_cwd(self, tmp_path):
         old_cwd = os.getcwd()
         try:
-            fs_root = pathlib.Path(fs, "foo")
+            fs_root = pathlib.Path(tmp_path, "foo")
             fs_root.mkdir()
             os.chdir(fs_root)
             program_root = pathlib.Path("programfoo")
 
             program = from_new_set_target_toolchain(program_root)
 
-            self.assertEqual(program.files, MbedProgramFiles.from_existing(program_root, DEFAULT_BUILD_SUBDIR))
+            assert program.files == MbedProgramFiles.from_existing(program_root, DEFAULT_BUILD_SUBDIR)
         finally:
             os.chdir(old_cwd)
 
-    @patchfs
-    def test_from_new_local_dir_generates_valid_program_existing_directory(self, fs):
-        fs_root = pathlib.Path(fs, "foo")
+    def test_from_new_local_dir_generates_valid_program_existing_directory(self, tmp_path):
+        fs_root = pathlib.Path(tmp_path, "foo")
         fs_root.mkdir()
         program_root = fs_root / "programfoo"
         program_root.mkdir()
 
         program = from_new_set_target_toolchain(program_root)
 
-        self.assertEqual(program.files, MbedProgramFiles.from_existing(program_root, DEFAULT_BUILD_SUBDIR))
+        assert program.files == MbedProgramFiles.from_existing(program_root, DEFAULT_BUILD_SUBDIR)
 
-    @patchfs
-    def test_from_existing_raises_if_path_is_not_a_program(self, fs):
-        fs_root = pathlib.Path(fs, "foo")
+    def test_from_existing_raises_if_path_is_not_a_program(self, tmp_path):
+        fs_root = pathlib.Path(tmp_path, "foo")
         fs_root.mkdir()
         program_root = fs_root / "programfoo"
 
-        with self.assertRaises(ProgramNotFound):
+        with pytest.raises(ProgramNotFound):
             MbedProgram.from_existing(program_root, DEFAULT_BUILD_SUBDIR)
 
-    @patchfs
-    def test_from_existing_raises_if_no_mbed_os_dir_found_and_check_mbed_os_is_true(self, fs):
-        fs_root = pathlib.Path(fs, "foo")
+    def test_from_existing_raises_if_no_mbed_os_dir_found_and_check_mbed_os_is_true(self, tmp_path):
+        fs_root = pathlib.Path(tmp_path, "foo")
         make_mbed_program_files(fs_root)
 
-        with self.assertRaises(MbedOSNotFound):
+        with pytest.raises(MbedOSNotFound):
             MbedProgram.from_existing(fs_root, DEFAULT_BUILD_SUBDIR, check_mbed_os=True)
 
-    @patchfs
-    def test_from_existing_returns_valid_program(self, fs):
-        fs_root = pathlib.Path(fs, "foo")
+    def test_from_existing_returns_valid_program(self, tmp_path):
+        fs_root = pathlib.Path(tmp_path, "foo")
         make_mbed_program_files(fs_root)
         make_mbed_os_files(fs_root / "mbed-os")
 
         program = MbedProgram.from_existing(fs_root, DEFAULT_BUILD_SUBDIR)
 
-        self.assertTrue(program.files.app_config_file.exists())
-        self.assertTrue(program.mbed_os.root.exists())
+        assert program.files.app_config_file.exists()
+        assert program.mbed_os.root.exists()
 
-    @patchfs
-    def test_from_existing_with_mbed_os_path_returns_valid_program(self, fs):
-        fs_root = pathlib.Path(fs, "foo")
+    def test_from_existing_with_mbed_os_path_returns_valid_program(self, tmp_path):
+        fs_root = pathlib.Path(tmp_path, "foo")
         mbed_os_path = fs_root / "extern/mbed-os"
         mbed_os_path.mkdir(parents=True)
         make_mbed_program_files(fs_root)
@@ -108,53 +100,50 @@ class TestInitialiseProgram(TestCase):
 
         program = MbedProgram.from_existing(fs_root, DEFAULT_BUILD_SUBDIR, mbed_os_path)
 
-        self.assertTrue(program.files.app_config_file.exists())
-        self.assertTrue(program.mbed_os.root.exists())
+        assert program.files.app_config_file.exists()
+        assert program.mbed_os.root.exists()
 
 
-class TestParseURL(TestCase):
+class TestParseURL:
     def test_creates_url_and_dst_dir_from_name(self):
         name = "mbed-os-example-blows-up-board"
         data = parse_url(name)
 
-        self.assertEqual(data["url"], f"https://github.com/armmbed/{name}")
-        self.assertEqual(data["dst_path"], name)
+        assert data["url"] == f"https://github.com/armmbed/{name}"
+        assert data["dst_path"] == name
 
     def test_creates_valid_dst_dir_from_url(self):
         url = "https://superversioncontrol/superorg/mbed-os-example-numskull"
         data = parse_url(url)
 
-        self.assertEqual(data["url"], url)
-        self.assertEqual(data["dst_path"], "mbed-os-example-numskull")
+        assert data["url"] == url
+        assert data["dst_path"] == "mbed-os-example-numskull"
 
     def test_creates_valid_dst_dir_from_ssh_url(self):
         url = "git@superversioncontrol:superorg/mbed-os-example-numskull"
         data = parse_url(url)
-        self.assertEqual(data["url"], url)
-        self.assertEqual(data["dst_path"], "mbed-os-example-numskull")
+        assert data["url"] == url
+        assert data["dst_path"] == "mbed-os-example-numskull"
 
 
-class TestFindProgramRoot(TestCase):
-    @patchfs
-    def test_finds_program_higher_in_dir_tree(self, fs):
-        program_root = pathlib.Path(fs, "foo")
+class TestFindProgramRoot:
+    def test_finds_program_higher_in_dir_tree(self, tmp_path):
+        program_root = pathlib.Path(tmp_path, "foo")
         pwd = program_root / "subprojfoo" / "libbar"
         make_mbed_program_files(program_root)
         pwd.mkdir(parents=True)
 
-        self.assertEqual(_find_program_root(pwd), program_root.resolve())
+        assert _find_program_root(pwd) == program_root.resolve()
 
-    @patchfs
-    def test_finds_program_at_current_path(self, fs):
-        program_root = pathlib.Path(fs, "foo")
+    def test_finds_program_at_current_path(self, tmp_path):
+        program_root = pathlib.Path(tmp_path, "foo")
         make_mbed_program_files(program_root)
 
-        self.assertEqual(_find_program_root(program_root), program_root.resolve())
+        assert _find_program_root(program_root) == program_root.resolve()
 
-    @patchfs
-    def test_raises_if_no_program_found(self, fs):
-        program_root = pathlib.Path(fs, "foo")
+    def test_raises_if_no_program_found(self, tmp_path):
+        program_root = pathlib.Path(tmp_path, "foo")
         program_root.mkdir()
 
-        with self.assertRaises(ProgramNotFound):
+        with pytest.raises(ProgramNotFound):
             _find_program_root(program_root)
