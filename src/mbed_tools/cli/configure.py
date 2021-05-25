ec48f5a708c239e9b9f7881cc8e2734f5d54c675
Gabriel Arjones <arjones@arjones.com>
2021-05-25 10:45:08 -0300

    config: Allow choosing output directory
    In production, builds are often triggered by third-party tools
    that sometimes manage build trees automatically. Choosing a different
    output directory for the generated mbed_config.json may benefit
    users in those situations.

--- src/mbed_tools/cli/configure.py
+++ src/mbed_tools/cli/configure.py
@@ -15,10 +15,7 @@ from mbed_tools.build import generate_config
     help="Generate an Mbed OS config CMake file and write it to a .mbedbuild folder in the program directory."
 )
 @click.option(
-    "--custom-targets-json",
-    type=click.Path(),
-    default=None,
-    help="Path to custom_targets.json.",
+    "--custom-targets-json", type=click.Path(), default=None, help="Path to custom_targets.json.",
 )
 @click.option(
     "-t",
@@ -28,6 +25,7 @@ from mbed_tools.build import generate_config
     help="The toolchain you are using to build your app.",
 )
 @click.option("-m", "--mbed-target", required=True, help="A build target for an Mbed-enabled device, eg. K64F")
+@click.option("-o", "--output-dir", type=click.Path(), default=None, help="Path to output directory.")
 @click.option(
     "-p",
     "--program-path",
@@ -38,7 +36,9 @@ from mbed_tools.build import generate_config
 @click.option(
     "--mbed-os-path", type=click.Path(), default=None, help="Path to local Mbed OS directory.",
 )
-def configure(toolchain: str, mbed_target: str, program_path: str, mbed_os_path: str, custom_targets_json: str) -> None:
+def configure(
+    toolchain: str, mbed_target: str, program_path: str, mbed_os_path: str, output_dir: str, custom_targets_json: str
+) -> None:
     """Exports a mbed_config.cmake file to build directory in the program root.
 
     The parameters set in the CMake file will be dependent on the combination of
@@ -54,6 +54,7 @@ def configure(toolchain: str, mbed_target: str, program_path: str, mbed_os_path:
         mbed_target: the target you are building for (eg. K64F)
         program_path: the path to the local Mbed program
         mbed_os_path: the path to the local Mbed OS directory
+        output_dir: the path to the output directory
     """
     cmake_build_subdir = pathlib.Path(mbed_target.upper(), "develop", toolchain.upper())
     if mbed_os_path is None:
@@ -62,6 +63,8 @@ def configure(toolchain: str, mbed_target: str, program_path: str, mbed_os_path:
         program = MbedProgram.from_existing(pathlib.Path(program_path), cmake_build_subdir, pathlib.Path(mbed_os_path))
     if custom_targets_json is not None:
         program.files.custom_targets_json = pathlib.Path(custom_targets_json)
+    if output_dir is not None:
+        program.files.cmake_build_dir = pathlib.Path(output_dir)
 
     mbed_target = mbed_target.upper()
     output_path = generate_config(mbed_target, toolchain, program)
