89f60b3065cb9e40bcd3674bc5a6339773a84881
Robert Walton <rob.walton@arm.com>
2021-07-28 21:17:24 +0100

    config: Remove support for target.requires
    target.requires was never a valid target_override. See
    https://github.com/ARMmbed/mbed-tools/issues/162 for details.
    
    Partially reverts 6a99b43.

--- src/mbed_tools/build/_internal/config/config.py
+++ src/mbed_tools/build/_internal/config/config.py
@@ -35,10 +35,6 @@ class Config(UserDict):
     def _handle_overrides(self, overrides: Iterable[Override]) -> None:
         for override in overrides:
             logger.debug("Applying override '%s.%s'='%s'", override.namespace, override.name, repr(override.value))
-            if override.name == "requires":
-                self.data["requires"] = self.data.get("requires", set()) | override.value
-                continue
-
             if override.name in self.data:
                 _apply_override(self.data, override)
                 continue
